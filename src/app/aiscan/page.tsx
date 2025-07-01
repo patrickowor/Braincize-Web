a0ea91bf43b4400846011faf018f6e8461bab363
Tollahh11 <omotolaboluwatife04@gmail.com>
2025-07-01 15:16:00 +0100

    edited styling for aiscan on desktop and mobile view

--- src/app/aiscan/page.tsx
+++ src/app/aiscan/page.tsx
@@ -1,4 +1,5 @@
 import aiStyle from "./aiscan.module.css"
+import Image from "next/image"
 
 export default function Aiscan(){
     return(<div className={aiStyle.aiscan}>
@@ -8,13 +9,17 @@ export default function Aiscan(){
             <button className={aiStyle.aiscanTopButton}>Learn More</button>
         </div>
         <div className={aiStyle.upload_container}>
-            <div className={aiStyle.upload_area} id="uploadArea">
-                <div className={aiStyle.upload_icon}><svg width="28" height="28" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z" />
-                    <path d="M12,11L16,15H13V19H11V15H8L12,11Z" />
-                </svg></div>
+            <div className={aiStyle.upload_area}>
+                <label id="uploadArea" htmlFor="fileInput">
+                <Image className={aiStyle.upload_icon}
+                    src={"/images/Vector.png"}
+                    alt="Braincize"
+                    width={100}
+                    height={109}
+                />
                 <div className={aiStyle.upload_text}>Click to upload or Drag and drop</div>
                 <div className={aiStyle.upload_subtext}>Support for multiple file types (PDF, DOC, TXT, etc.)</div>
+                </label>
                 <input type="file" id="fileInput" className={aiStyle.file_input} multiple accept=".pdf,.doc,.docx,.txt,.jpg,.jpeg,.png"/>
             </div>
             <button className={aiStyle.fileButton}>AI Scan</button>
