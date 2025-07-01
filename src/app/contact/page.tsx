2306f3e0eafa81aabdee9e0f4a7beb9914f03cbe
Tollahh11 <omotolaboluwatife04@gmail.com>
2025-07-01 13:43:59 +0100

    edited contact page on desktop and mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,20 +3,20 @@ import contactStyle from "./contact.module.css"
 export default function Contact() {
     return (<>
         <div className={contactStyle.contactpage}>
-            <h2 className={contactStyle.contactpage_h2}>Contact us</h2>
-            <div className={contactStyle.contactpage_p}>
+            <h2 className={`fnt-monserat ${contactStyle.contactpage_h2}`}>Contact us</h2>
+            <div className={`fnt-poppings ${contactStyle.contactpage_p}`}>
                 <p>Social links and articles</p>
                 <p>Twitter Research gate Legal Expert</p>
             </div>
         </div>
         <div>
-            <form action="" className={contactStyle.formm}>
+            <form action="" className={`fnt-poppings ${contactStyle.formm}`}>
                 <input type="text" placeholder="Your Name" className={contactStyle.formm_maininput}/>
                 <input type="email" placeholder="Your Email"className={contactStyle.formm_maininput}/>
                 <input type="text" placeholder="Your Address"className={contactStyle.formm_maininput}/>
                 <input type="number" placeholder="Your Number"className={contactStyle.formm_maininput}/>
                 <textarea placeholder="Write Something" className={contactStyle.formm_textarea}></textarea>
-                <button className={contactStyle.formm_button }>Contact Us</button>
+                <button className={`fnt-poppings ${contactStyle.formm_button }`}>Contact Us</button>
             </form>
         </div>
     </>);
